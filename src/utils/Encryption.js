64f6fdfd27bb9b546a9f7a2b522767901c091507
Sushant Gupta <sushantgupta2016@gmail.com>
2022-08-27 13:02:55 -0500

    Add secrets to firebase hosting template

--- src/utils/Encryption.js
+++ src/utils/Encryption.js
@@ -5,12 +5,7 @@ const keySalt = process.env.REACT_APP_ENCRYPTION_SALT;
 export const encrypt = (inputText, userKey) => {
   const plainText = JSON.stringify(inputText);
   const key = userKey + keySalt;
-  console.log(plainText);
-  console.log(userKey);
-  console.log(keySalt);
-  console.log(key);
   const encrypted = CryptoJS.AES.encrypt(plainText, key).toString();
-  console.log(encrypted);
   const base64Encoded = CryptoJS.enc.Base64.stringify(
     CryptoJS.enc.Utf8.parse(encrypted)
   );
@@ -21,9 +16,9 @@ export const decrypt = (inputText, userKey) => {
   const base64Decoded = CryptoJS.enc.Base64.parse(inputText).toString(
     CryptoJS.enc.Utf8
   );
-  const decrytedObj = CryptoJS.AES.decrypt(
-    base64Decoded,
-    userKey + keySalt
-  ).toString(CryptoJS.enc.Utf8);
+  const key = userKey + keySalt;
+  const decrytedObj = CryptoJS.AES.decrypt(base64Decoded, key).toString(
+    CryptoJS.enc.Utf8
+  );
   return JSON.parse(decrytedObj);
 };
