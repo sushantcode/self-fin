a65d45965ec5d63c2681f64b30c845822895fb23
sushantcode <sushantgupta@Sushants-MacBook-Pro.local>
2023-02-18 18:48:42 -0800

    Add footer

--- src/App.js
+++ src/App.js
@@ -12,28 +12,32 @@ import Report from './components/pages/report/Report';
 import Savings from './components/pages/savings/Savings';
 import Titlebar from './components/titlebar/Titlebar';
 import BatchProcessor from './components/pages/batch_processor/BatchProcessor';
+import Footer from './components/footer/Footer';
 
 function App() {
     return (
         <Router>
-            <Titlebar />
-            <Container>
-                <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/expense" element={<Expense />} />
-                    <Route path="/income" element={<Income />} />
-                    <Route path="/loan" element={<Loan />} />
-                    <Route path="/savings" element={<Savings />} />
-                    <Route path="/investments" element={<Investments />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/report" element={<Report />} />
-                    <Route
-                        path="/batchProcessor"
-                        element={<BatchProcessor />}
-                    />
-                </Routes>
-            </Container>
+            <div className="d-flex flex-column min-vh-100">
+                <Titlebar />
+                <Container className="flex-grow-1">
+                    <Routes>
+                        <Route path="/" element={<Dashboard />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/expense" element={<Expense />} />
+                        <Route path="/income" element={<Income />} />
+                        <Route path="/loan" element={<Loan />} />
+                        <Route path="/savings" element={<Savings />} />
+                        <Route path="/investments" element={<Investments />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/report" element={<Report />} />
+                        <Route
+                            path="/batchProcessor"
+                            element={<BatchProcessor />}
+                        />
+                    </Routes>
+                </Container>
+                <Footer />
+            </div>
         </Router>
     );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+const Footer = () => {
+    return (
+        <footer className="bg-light py-3 mt-auto">
+            <Container>
+                <Row>
+                    <Col className="container text-center">
+                        <p className="mb-0">
+                            &copy; 2023 Gupta, Sushant. All rights reserved.
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+};
+
+export default Footer;
